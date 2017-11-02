c39363fda6a5679f48ca4453954adeab8d63b179
Bob <radean@hotmail.com>
2017-11-02 14:55:48 +0000

    initial commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,9 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+#
+#
+
+
+include_recipe 'apache::server'
+ 
---
+++ recipes/server.rb
@@ -0,0 +1,18 @@
+#
+# Cookbook:: apache
+# Recipe:: server
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
+#
+
+
+package 'httpd'
+
+template '/var/www/html/index.html' do
+	source 'index.html.erb'
+end
+
+service 'httpd' do
+	action [:start, :enable]
+end
+
